427c2bb4ebe3059433b254d7f126b20d8b179b59
Francesco-Zac <francescozaccaro2@gmail.com>
2025-06-18 16:43:57 +0200

    add changes

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,18 @@
 import entities.SIM;
-import entities.Articoli;
-import entities.Cliente;
-import entities.Carrello;
+
 
 public class Main {
     public static void main(String[] args) {
         SIM mioNumero = new SIM(0525252525);
 
-//        mioNumero.stampaDettagliSIM();
-//        mioNumero.ricarica();
-//        mioNumero.stampaDettagliSIM();
-//        mioNumero.aggiungiChiamata("0254565852", "5");
-//        mioNumero.stampaDettagliSIM();
-//        mioNumero.aggiungiChiamata("1254565852", "3");
-//        mioNumero.stampaDettagliSIM();
+        mioNumero.stampaDettagliSIM();
+        mioNumero.ricarica();
+        mioNumero.stampaDettagliSIM();
+        mioNumero.aggiungiChiamata("0254565852", "5");
+        mioNumero.stampaDettagliSIM();
+        mioNumero.aggiungiChiamata("1254565852", "3");
+        mioNumero.stampaDettagliSIM();
 
-        Cliente cliente = new Cliente("CL001", "Mario", "Rossi", "mario@example.com", "2024-06-01");
 
-        Articoli articolo1 = new Articoli(1001, "Mouse Wireless", 25, 10);
-        Articoli articolo2 = new Articoli(1002, "Tastiera Meccanica", 50, 5);
-
-        int[] codiciArticoli = {articolo1.codiceArticolo, articolo2.codiceArticolo};
-        int costoTotale = articolo1.prezzo + articolo2.prezzo;
-
-        Carrello carrello = new Carrello(cliente.codiceCliente, codiciArticoli, costoTotale);
-
-        System.out.println("Cliente: " + cliente.nome + " " + cliente.cognome);
-        System.out.println("Articoli nel carrello:");
-        for (int codice : carrello.codiciArticoli) {
-            if (codice == articolo1.codiceArticolo) {
-                System.out.println("- " + articolo1.descrizioneArticolo + ": " + articolo1.prezzo + "€");
-            } else if (codice == articolo2.codiceArticolo) {
-                System.out.println("- " + articolo2.descrizioneArticolo + ": " + articolo2.prezzo + "€");
-            }
-        }
-        System.out.println("Totale: " + carrello.costoTotale + "€");
-    }
     }
 }
--- src/entities/Carrello.java
+++ src/entities/Carrello.java
@@ -1,11 +1,11 @@
 package entities;
 
 public class Carrello {
-    public Cliente clienteAssociato;
+    public String clienteAssociato;
     public String[] elencoArticoli;
     public int costoTotale;
 
-    public Carrello(Cliente clienteAssociato, String[] elencoArticoli, int costoTotale) {
+    public Carrello(String clienteAssociato, String[] elencoArticoli, int costoTotale) {
         this.clienteAssociato = clienteAssociato;
         this.elencoArticoli = elencoArticoli;
         this.costoTotale = costoTotale;
